a11e18c113ac7dbe88d311cccd3546fd8c040f6a
ftmzhrsckldr <ftmzhrsckldr@protonmail.com>
2023-10-06 10:22:13 +0300

    Fixed output problem

--- main.sh
+++ main.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
-set -e
+set -ex
+
+APPDOME_APP_PATH=$AC_APPDOME_APP_PATH
+APPDOME_API_KEY=$AC_APPDOME_API_KEY
+APPDOME_FUSION_SET_ID=$AC_APPDOME_FUSION_SET_ID
+APPDOME_TEAM_ID=$AC_APPDOME_TEAM_ID
+APPDOME_SIGN_METHOD=$AC_APPDOME_SIGN_METHOD
+APPDOME_GP_SIGNING=$AC_APPDOME_GP_SIGNING
+APPDOME_BUILD_LOGS=$AC_APPDOME_BUILD_LOGS
+ANDROID_KEYSTORE_PATH=$AC_ANDROID_KEYSTORE_PATH
 
 download_file() {
 	file_location=$1
@@ -9,32 +18,24 @@ download_file() {
 	curl -L $file_location --output $downloaded_file && echo $downloaded_file
 }
 
-if [[ -z $APPDOME_API_KEY ]]; then
-	echo 'APPDOME_API_KEY must be provided as a Secret. Exiting.'
-	exit 1
-fi
-
-if [[ $AC_APPDOME_APP_PATH == *"http"* ]];
+if [[ $APPDOME_APP_PATH == *"http"* ]];
 then
-	app_file=../$(download_file $AC_APPDOME_APP_PATH)
+	app_file=AC_REPOSITORY_DIR/$(download_file $APPDOME_APP_PATH)
 else
-	app_file=$AC_APPDOME_APP_PATH
+	app_file=$APPDOME_APP_PATH
 fi
-certificate_output=../certificate.pdf
-secured_app_output=../Appdome_$(basename $app_file)
-
+certificate_output=AC_REPOSITORY_DIR/certificate.pdf
+secured_app_output=AC_REPOSITORY_DIR/Appdome_$(basename $app_file)
 
 tm=""
-if [[ -n $AC_APPDOME_TEAM_ID ]]; then
+if [[ -n $APPDOME_TEAM_ID ]]; then
 	tm="--team_id ${team_id}"
 fi
 
-cp "$AC_ANDROID_KEYSTORE_PATH" "$AC_ANDROID_KEYSTORE_PATH.keystore"
+cp "$ANDROID_KEYSTORE_PATH" "$ANDROID_KEYSTORE_PATH.keystore"
 
 git clone https://github.com/Appdome/appdome-api-bash.git > /dev/null
 cd appdome-api-bash
-
-echo "Android platform detected"
 
 cf=""
 if [[ -n $SIGN_FINGERPRINT ]]; then
@@ -42,7 +43,7 @@ if [[ -n $SIGN_FINGERPRINT ]]; then
 fi
 
 gp=""
-if [[ $AC_APPDOME_GP_SIGNING == "true" ]]; then
+if [[ $APPDOME_GP_SIGNING == "true" ]]; then
 	gp="--google_play_signing"
 	if [[ -z $AC_GOOGLE_SIGN_FINGERPRINT ]]; then
 		echo "AC_GOOGLE_SIGN_FINGERPRINT must be provided as a Secret for Google Play signing. Exiting."
@@ -52,15 +53,15 @@ if [[ $AC_APPDOME_GP_SIGNING == "true" ]]; then
 fi
 
 bl=""
-if [[ $AC_APPDOME_BUILD_LOGS == "true" ]]; then
+if [[ $APPDOME_BUILD_LOGS == "true" ]]; then
 	bl="-bl"
 fi
 
-case $AC_APPDOME_SIGN_METHOD in
+case $APPDOME_SIGN_METHOD in
 "Private-Signing")		echo "Private Signing"
 						./appdome_api.sh --api_key $APPDOME_API_KEY \
 							--app $app_file \
-							--fusion_set_id $AC_APPDOME_FUSION_SET_ID \
+							--fusion_set_id $APPDOME_FUSION_SET_ID \
 							$tm \
 							--private_signing \
 							$gp \
@@ -72,7 +73,7 @@ case $AC_APPDOME_SIGN_METHOD in
 "Auto-Dev-Signing")		echo "Auto Dev Signing"
 						./appdome_api.sh --api_key $APPDOME_API_KEY \
 							--app $app_file \
-							--fusion_set_id $AC_APPDOME_FUSION_SET_ID \
+							--fusion_set_id $APPDOME_FUSION_SET_ID \
 							$tm \
 							--auto_dev_private_signing \
 							$gp \
@@ -82,13 +83,13 @@ case $AC_APPDOME_SIGN_METHOD in
 							--certificate_output $certificate_output 
 						;;
 "On-Appdome")			echo "On Appdome Signing"
-						keystore_file="$AC_ANDROID_KEYSTORE_PATH.keystore"
+						keystore_file="$ANDROID_KEYSTORE_PATH.keystore"
 						keystore_pass=$AC_ANDROID_KEYSTORE_PASSWORD
 						keystore_alias=$AC_ANDROID_ALIAS
 						key_pass=$AC_ANDROID_ALIAS_PASSWORD
 						./appdome_api.sh --api_key $APPDOME_API_KEY \
 							--app $app_file \
-							--fusion_set_id $AC_APPDOME_FUSION_SET_ID \
+							--fusion_set_id $APPDOME_FUSION_SET_ID \
 							$tm \
 							--sign_on_appdome \
 							--keystore $keystore_file \
@@ -103,8 +104,7 @@ case $AC_APPDOME_SIGN_METHOD in
 						;;
 esac
 
-secured_app_output="$AC_REPOSITORY_DIR/$(basename $secured_app_output)"
-certificate_output="$AC_REPOSITORY_DIR/$(basename $certificate_output)"
+echo "Outputs are being prepared."
 
 if [[ $secured_app_output == *.sh ]]; then
 	echo "AC_APPDOME_PRIVATE_SIGN_SCRIPT_PATH=$secured_app_output" >> $AC_ENV_FILE_PATH
@@ -114,5 +114,9 @@ elif [[ $secured_app_output == *.aab ]]; then
 	echo "AC_APPDOME_SECURED_AAB_PATH=$secured_app_output" >> $AC_ENV_FILE_PATH
 else
 	echo "Secured app output is undefined: $secured_app_output"
+	exit 1
 fi
+
+cp $secured_app_output $AC_OUTPUT_DIR
+cp $certificate_output $AC_OUTPUT_DIR
 echo "AC_APPDOME_CERTIFICATE_PATH=$certificate_output" >> $AC_ENV_FILE_PATH
